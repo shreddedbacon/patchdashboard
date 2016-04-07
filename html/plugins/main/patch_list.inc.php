40d177679f4fc136c6abb3f4e9c7f12372aaf5e3
Ben Jackson <bacon@shreddedbacon.com>
2016-04-07 19:26:43 +1000

    use bootstrap label for patch list table urgency

--- html/plugins/main/patch_list.inc.php
+++ html/plugins/main/patch_list.inc.php
@@ -54,16 +54,20 @@ if (!isset($index_check) || $index_check != "active"){
                 }
         }
      if (in_array($urgency,array('high','emergency'))){
-                $urgency = "<td style='color:red'><a href='http://www.ubuntuupdates.org/package/core/trusty/main/updates/$package_name_orig' style='color:red' target='_blank'>$urgency</a></td>";
+                //$urgency = "<td style='color:red'><a href='http://www.ubuntuupdates.org/package/core/trusty/main/updates/$package_name_orig' style='color:red' target='_blank'>$urgency</a></td>";
+                $urgency = '<td><span class="label label-danger">'.$urgency.'</span></td>';
      }
      elseif ($urgency == "medium"){
-                $urgency = "<td style='color:#FF8C00'><a href='http://www.ubuntuupdates.org/package/core/trusty/main/updates/$package_name_orig' style='color:#FF8C00' target='_blank'>medium</a></td>";
+                //$urgency = "<td style='color:#FF8C00'><a href='http://www.ubuntuupdates.org/package/core/trusty/main/updates/$package_name_orig' style='color:#FF8C00' target='_blank'>medium</a></td>";
+                $urgency = '<td><span class="label label-warning">'.$urgency.'</span></td>';
      }
      elseif ($urgency == "low") {
-                $urgency = "<td><a href='http://www.ubuntuupdates.org/package/core/trusty/main/updates/$package_name_orig' style='color:black' target='_blank'>$urgency</a></td>";
+                //$urgency = "<td><a href='http://www.ubuntuupdates.org/package/core/trusty/main/updates/$package_name_orig' style='color:black' target='_blank'>$urgency</a></td>";
+                $urgency = '<td><span class="label label-info">'.$urgency.'</span></td>';
      }
      else{
-                $urgency = "<td>$urgency</td>";
+                //$urgency = "<td>$urgency</td>";
+                $urgency = '<td><span class="label label-primary">'.$urgency.'</span></td>';
      }
      $table .= "                <tr>
                   <td><input type='checkbox' name='patch_id[]' value='".$row1['id']."'>

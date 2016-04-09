85906374ea59b6c8be26e7382d1cd8cbb0a4f0e1
Ben Jackson <bacon@shreddedbacon.com>
2016-04-09 12:32:09 +1000

    change to db_config to use database company name

---
+++ html/lib/company_config.php
@@ -0,0 +1,7 @@
+<?php
+$link = mysql_connect(DB_HOST,DB_USER,DB_PASS);
+mysql_select_db(DB_NAME,$link);
+$sql = "SELECT * FROM company;";
+$res = mysql_query($sql);
+$row = mysql_fetch_assoc($res);
+define('YOUR_COMPANY',$row['display_name']);

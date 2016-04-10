efa90ee6e217b605e66b11b2092034b15c0ba044
Ben Jackson <bacon@shreddedbacon.com>
2016-04-10 11:32:53 +1000

    minor changes and new favicon

--- html/inc/header.inc.php
+++ html/inc/header.inc.php
@@ -76,7 +76,7 @@ foreach ($navbar_array as $key=>$val){
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="shortcut icon" href="<?php echo BASE_PATH; ?>favicon.ico">
+  <link rel="shortcut icon" href="<?php echo BASE_PATH; ?>favicon.png">
   <title>Patch Management Dashboard </title>
   <!-- Bootstrap core CSS -->
   <link href="<?php echo BASE_PATH;?>css/bootstrap.min.css" rel="stylesheet">
--- html/plugins/main/patches.inc.php
+++ html/plugins/main/patches.inc.php
@@ -60,13 +60,13 @@ if (!isset($index_check) || $index_check != "active"){
      $total_count = $total_count + $count;
      if ($count == 0) {
      $table .= "                <tr>
-                  <td><a href='{$base_path}patches/server/$server_name'><img src='$dist_img' height='32' width='32' border='0'>&nbsp;$server_alias</a></td>
+                  <td><img src='$dist_img' class='avatar'>&nbsp;<a href='{$base_path}patches/server/$server_name'>$server_alias</a></td>
                   <td><span class='label label-success'>Up to date :)</span></td>
                 </tr>
 ";
      } else {
      $table .= "                <tr>
-                  <td><a href='{$base_path}patches/server/$server_name'><img src='$dist_img' height='32' width='32' border='0'>&nbsp;$server_alias</a></td>
+                  <td><img src='$dist_img' class='avatar'>&nbsp;<a href='{$base_path}patches/server/$server_name'>$server_alias</a></td>
                   <td><span class='label label-default'>Total $count</span> | <span class='label label-danger'>High $count4</span> | <span class='label label-warning'>Medium $count2</span> | <span class='label label-info'>Low $count3</span> | <span class='label label-primary'>Unknown $count5</span></td>
                 </tr>
 ";

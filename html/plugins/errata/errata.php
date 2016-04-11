a9317d7feb792ee50a706637ab1503f70a2a5cd9
Ben Jackson <bacon@shreddedbacon.com>
2016-04-11 20:43:47 +1000

    testing errata plugin

---
+++ html/plugins/errata/errata.php
@@ -0,0 +1,12 @@
+<?php
+
+$xml_string = file_get_contents('<?php echo BASE_PATH;?>plugins/errata/xml.xml');
+
+$xml = simplexml_load_string($xml_string);
+$json = json_encode($xml);
+$array = json_decode($json,TRUE);
+
+echo $json;
+print_r($array);
+
+?> 

5b07b8f8ad441f768dac7d3182a24cdb15bbdc30
Ben Jackson <bacon@shreddedbacon.com>
2016-04-28 16:31:43 +1000

    fixed up editing services

--- html/plugins/admin/p_edit_services.inc.php
+++ html/plugins/admin/p_edit_services.inc.php
@@ -6,8 +6,8 @@ if (isset($_SESSION['is_admin']) && $_SESSION['is_admin'] == true) {
         $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
         $service_name = filter_input(INPUT_POST, 'service_name', FILTER_SANITIZE_SPECIAL_CHARS);
         $service_cmd = filter_input(INPUT_POST, 'service_cmd', FILTER_SANITIZE_SPECIAL_CHARS);
-        $server_ids = filter_input_array(INPUT_POST, 'server_id', FILTER_SANITIZE_SPECIAL_CHARS);
-        $sql_array = array();
+        $server_ids = $_POST['server_id']; //filter_input_array(INPUT_POST, 'server_id', FILTER_SANITIZE_SPECIAL_CHARS);
+        
         if (isset($service_name) && !empty($service_name) && isset($id) && !empty($id)){
             $replacement_parts = implode(", ", $sql_array);
             $sql = "UPDATE `service_list` SET service_name='$service_name', service_cmd='$service_cmd' WHERE `id`=$id LIMIT 1;";
@@ -15,7 +15,18 @@ if (isset($_SESSION['is_admin']) && $_SESSION['is_admin'] == true) {
             mysql_select_db(DB_NAME,$link);
             mysql_query($sql);
 			
+			$sql_del = "DELETE FROM `services` WHERE `service_id`=$id;";
+			mysql_query($sql_del);
+			
+			//$sql_add = print_r($server_ids);
+			for ($i=0;$i<count($server_ids);$i++) {
+				$server_id = $server_ids[$i];
+				$sql_add = "INSERT INTO `services` (`server_id`,`service_id`) VALUES ($server_id,$id);";
+				mysql_query($sql_add);
+			}		
+			
             mysql_close($link);
+			
             $_SESSION['good_notice'] = "$service_name modified! That wasn't so bad, now was it?";
             sleep(1);
             header('location:'.BASE_PATH."edit_services?id=$id");
